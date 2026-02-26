9b3220c9b60485cf2cb32081de817ce7fa80d14b
wae <wawa>
2026-02-26 11:54:59 +0100

    svg z2

--- svg/src/Main.java
+++ svg/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        Point point = new Point();
+        point.x = 5.4F;
+        point.y = 3.3F;
+        System.out.println(point.toSvg());;
     }
 }
