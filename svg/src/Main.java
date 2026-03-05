879d44618022dfa14b617e94c1af3845c2c7c776
wae <wawa>
2026-03-05 11:21:27 +0100

    svg z1

--- svg/src/Main.java
+++ svg/src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        Point p1 = new Point(3,6);
-        Point p2 = new Point(2,7);
+        Point[] points = new Point[5];
+        points[0] = new Point (5.5F,63.2F);
+        points[1] = new Point (24.8F,6.5F);
+        points[2] = new Point (356.2F,10.2F);
+        points[3] = new Point (28.9F,24F);
+        points[4] = new Point (252.4F,62.7F);
 
-        Segment s1 = new Segment(new Point(p1),p2);
-
-        System.out.println(s1);
-        p1.setX(5);
-        System.out.println(p1);
-        System.out.println(s1);
+        Polygon polygon = new Polygon(points);
+        System.out.println(polygon.toSvg());
     }
 }
