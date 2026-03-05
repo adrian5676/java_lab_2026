326def1f82c60f6c1e38df992c03bd6354f1f1c8
wae <wawa>
2026-03-05 10:25:53 +0100

    svg z4

--- svg/src/Point.java
+++ svg/src/Point.java
@@ -3,7 +3,14 @@ import java.util.Locale;
 public class Point {
     private float x;
     private float y;
-
+    public Point(float x, float y){
+        this.x=x;
+        this.y=y;
+    }
+    public Point(){
+        this.x=0.0f;
+        this.y=0.0f;
+    }
     public void setX(float x){
         this.x=x;
     }
@@ -16,6 +23,7 @@ public class Point {
     public float getY(){
         return y;
     }
+
 
     @Override
     public String toString() {
