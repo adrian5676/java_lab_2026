4e09db48910bc0919b106b369344e6923667adcd
wae <wawa>
2026-04-09 10:47:00 +0200

    ratuj

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,87 @@
-public class Person {
+import java.time.LocalDate;
+import java.util.*;
+
+public class Person implements Comparable<Person> {
+    private final String  firstName;
+    private final String lastName;
+    private final LocalDate birthday;
+
+    private final Set<Person> children = new HashSet<>();
+
+    public Person(String firstName, String lastName, LocalDate birthday) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthday=" + birthday +
+                ", children=" + children +
+                '}';
+    }
+
+    public boolean adopt(Person child){
+        if (child == this) return false;
+        return children.add(child);
+    }
+/*
+    public Person getYoungestChild() {
+        if (this.children.isEmpty()) {
+            return null;
+        }
+        Iterator<Person> iter = this.children.iterator();
+        Person now = iter.next();
+        Person youngest = now;
+        while (true) {
+            if (youngest.birthday.compareTo(now.birthday)>0) {
+                youngest=now;
+            }
+            try {
+                now = iter.next();
+            } catch (NoSuchElementException e) {
+                break;
+            }
+        }
+        return youngest;
+    }
+
+ */
+
+    public Person getYoungestChild() {
+        if (this.children.isEmpty()) {
+            return null;
+        }
+        Person youngest = children.iterator().next();
+        for(Person person: children) {
+            if(youngest.compareTo(person)>0)
+                youngest = person;
+        }
+        return youngest;
+    }
+
+    public List<Person> getChildren() {
+        List<Person> result = new ArrayList<>();
+        result.addAll(children);
+        result.sort(Person::compareTo);
+        return result;
+
+        //return children.stream().sorted().toList();
+
+        //List<Person> result = new ArrayList<>(children.stream().toList());
+        //result.sort(Person::compareTo);
+        //return result;
+    }
+
+    public String name() {
+        return String.format("%s %s",firstName, lastName);
+    }
+
+    @Override
+    public int compareTo(Person other) {
+        return this.birthday.compareTo(other.birthday);
+    }
 }
