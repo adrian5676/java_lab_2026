4f3041873d4b9aa66cd58211f5790f55d0c8fd82
wae <wawa>
2026-05-21 11:17:58 +0200

    music z1f

--- src/main/java/music/Playlist.java
+++ src/main/java/music/Playlist.java
@@ -4,5 +4,16 @@ import java.util.ArrayList;
 
 public class Playlist extends ArrayList<Song> {
 
-
+    public Song atSecond(int second){
+        int offSet = 0;
+        for (Song song : this){
+            int end = offSet + song.getLength();
+            if (offSet <= second && second < end){
+                return song;
+            } else {
+                offSet = end;
+            }
+        }
+        return null;
+    }
 }
--- src/test/java/music/PlaylistTest.java
+++ src/test/java/music/PlaylistTest.java
@@ -32,6 +32,19 @@ public class PlaylistTest {
         playlist.add(song);
         assertEquals(playlist.get(0), song2);;
         assertTrue(playlist.contains(song));
-
+    }
+    @Test
+    public void testAtSecond(){
+        Playlist playlist = new Playlist();
+        Song song1 = new Song("Iron Maiden", "Killers", 260);
+        Song song2 = new Song( "Megadeth", "Dawn Patrol", 120);
+        Song song3 = new Song("Darkthrone", "Natassja in Eternal Sleep", 250);
+        playlist.add(song1);
+        playlist.add(song2);
+        playlist.add(song3);
+        assertEquals(song1, playlist.atSecond(0));
+        assertEquals(song1, playlist.atSecond(50));
+        assertEquals(song2, playlist.atSecond(300));
+        assertEquals(song3, playlist.atSecond(400));
     }
 }
